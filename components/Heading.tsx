a48590d3ec230d71c4f456bb22c9b1e517f8733e
SofiaLoven <sofia.loven93@gmail.com>
2024-07-01 17:21:21 +0200

    Fix download cv

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import profilePicture from "@/public/images/Profile2.png";
 
 const Heading = () => {
@@ -22,9 +23,12 @@ const Heading = () => {
           </span>
         </div>
         <div>
-          <a
+          <Link
             href="/CV_Loven_Sofia.pdf"
-            download="CV_Loven_Sofia"
+            target="blank"
+            rel="noopener noreferrer"
+            locale="false"
+            download
             className="flex gap-1 md:gap-4 items-center hover:bg-green-200 bg-green-300 hover:-translate-y-1 hover:shadow-lg rounded-full text-sm px-4 py-2 text-center transition ease-in-out delay-150 duration-300 size-fit"
           >
             Download resume
@@ -42,7 +46,7 @@ const Heading = () => {
                 d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
               />
             </svg>
-          </a>
+          </Link>
         </div>
       </div>
       <Image
